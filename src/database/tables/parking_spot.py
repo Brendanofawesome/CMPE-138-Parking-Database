d668df3c77d51ac056c5f8980dccb21d55745039
Brendan <brendanparvin7@gmail.com>
2026-04-21 16:14:06 -0700

    Created the parking spot schema

---
+++ src/database/tables/parking_spot.py
@@ -0,0 +1,23 @@
+from ..establish_db import SQLColumn, Table, register_table
+
+###############
+# SPOT SCHEMA #
+###############
+register_table(
+    Table(name='parking_spot',
+        columns=(
+            SQLColumn(name='location_id', attribute_list='NOT NULL'),
+            SQLColumn(name='spot_number', attribute_list='INTEGER NOT NULL'),
+            SQLColumn(name='active', attribute_list='BOOLEAN NOT NULL'),
+            SQLColumn(name='location_description', attribute_list='TEXT'),
+            SQLColumn(name='type', attribute_list="TEXT NOT NULL"),
+            SQLColumn(name='loc_x_min', attribute_list='INTEGER NOT NULL'),
+            SQLColumn(name='loc_x_max', attribute_list='INTEGER NOT NULL'),
+            SQLColumn(name='box_y_min', attribute_list='INTEGER NOT NULL'),
+            SQLColumn(name='box_y_max', attribute_list='INTEGER NOT NULL'),
+            SQLColumn(name='in_use', attribute_list="AS (SELECT EXISTS (SELECT 1 FROM fees WHERE expiry_time > unixepoch()))")
+        ),
+        extra_constraints=("FOREIGN KEY (location_id) REFERENCES locations(location_id) ON DELETE CASCADE",
+                           "PRIMARY KEY (location_id, spot_number)") 
+    )
+)
