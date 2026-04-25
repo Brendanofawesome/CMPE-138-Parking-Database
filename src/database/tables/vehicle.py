d8063f465a164ba753d4f24a0e1e09b7b0cabaad
kphamcodes <kpham120903@gmail.com>
2026-04-25 15:36:44 -0700

    Update vehicle table columns and constraints
    minor attribute list fix

--- src/database/tables/vehicle.py
+++ src/database/tables/vehicle.py
@@ -3,14 +3,14 @@ from ..establish_db import SQLColumn, Table, register_table
 register_table(
     Table(name='vehicle', 
         columns=(
-            SQLColumn(name='Licence_Value', attribute_list='TEXT'),
-            SQLColumn(name='Licence_State', attribute_list='TEXT'),
-            SQLColumn(name='Make', attribute_list='TEXT'),
+            SQLColumn(name='License_Value', attribute_list='TEXT NOT NULL'),
+            SQLColumn(name='License_State', attribute_list='TEXT NOT NULL'),
+            SQLColumn(name='Make', attribute_list='TEXT NOT NULL'),
             SQLColumn(name='Color', attribute_list='TEXT'),
-            SQLColumn(name='Model', attribute_list='Text'),
-            SQLColumn(name="user_id", attribute_list="INTEGER")
+            SQLColumn(name='Model', attribute_list='TEXT NOT NULL'),
+            SQLColumn(name="user_id", attribute_list="INTEGER NOT NULL")
         ),
-        primary_key=("Licence_Value", "Licence_State", "user_id"),
+        primary_key=("License_Value", "License_State", "user_id"),
         extra_constraints=(
             SQLColumn(name='fk_vehicle_user', attribute_list='FOREIGN KEY (user_id) REFERENCES user(user_id) ON DELETE CASCADE'),
         )
