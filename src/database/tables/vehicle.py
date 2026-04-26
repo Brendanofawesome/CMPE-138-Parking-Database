7dbf141176bf925dddf0abb377364b3f8a450c17
KPhamCodes <kpham120903@gmail.com>
2026-04-25 17:26:01 -0700

    removed 'vehicle_id'
    added composite PK 'license_plate' consisting of 'license_plate_value, license_plate_state'

--- src/database/tables/vehicle.py
+++ src/database/tables/vehicle.py
@@ -1,18 +1,25 @@
 from ..establish_db import SQLColumn, Table, register_table
 
+##################
+# VEHICLE SCHEMA #
+##################
+
 register_table(
-    Table(name='vehicle', 
+    Table(
+        name='vehicle',
         columns=(
-            SQLColumn(name='License_Value', attribute_list='TEXT NOT NULL'),
-            SQLColumn(name='License_State', attribute_list='TEXT NOT NULL'),
-            SQLColumn(name='Make', attribute_list='TEXT NOT NULL'),
-            SQLColumn(name='Color', attribute_list='TEXT'),
-            SQLColumn(name='Model', attribute_list='TEXT NOT NULL'),
-            SQLColumn(name="user_id", attribute_list="INTEGER NOT NULL")
+            SQLColumn(name='user_id', attribute_list='INTEGER NOT NULL'),
+            SQLColumn(name='color', attribute_list='TEXT'),
+            SQLColumn(name='make', attribute_list='TEXT NOT NULL'),
+            SQLColumn(name='model', attribute_list='TEXT NOT NULL'),
+            SQLColumn(name='license_plate_value', attribute_list='TEXT NOT NULL'),
+            SQLColumn(name='license_plate_state', attribute_list='TEXT NOT NULL')
         ),
-        primary_key=("License_Value", "License_State", "user_id"),
+        primary_key=("license_plate", "license_plate_value", "licence_plate_state", "user_id"),
+
         extra_constraints=(
             SQLColumn(name='fk_vehicle_user', attribute_list='FOREIGN KEY (user_id) REFERENCES user(user_id) ON DELETE CASCADE'),
+            SQLColumn(name='license_plate', attribute_list='UNIQUE (license_plate_value, license_plate_state)')
         )
     )
 )
